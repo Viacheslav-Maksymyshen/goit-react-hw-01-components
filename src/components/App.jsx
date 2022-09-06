040772f0cd62a15f5e12a5d15cba6ed1b6f27bc2
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-09-06 21:19:25 +0300

    creating a profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import Profile from './Profile/Profile';
+
+import user from '../data/user.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile user={user} />
     </div>
   );
 };
